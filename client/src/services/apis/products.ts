13feba7fd3bae9434b2abbcd5a0fd91973bbab55
asol25 <usool.203@gmail.com>
2022-12-21 16:01:21 +0700

    Modify ENV production

--- client/src/services/apis/products.ts
+++ client/src/services/apis/products.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getProducts = async (_page: number, _limit: number) => {
 	const products = await axios.get(
 		`${
-			process.env.REACT_APP_VERCEL_ENV_AUTH0_DOMAIN || "http://localhost:33714"
+			process.env.REACT_APP_VERCEL_ENV_API_DOMAIN || "http://localhost:33714"
 		}/products/pagination/${_page}/${_limit}`
 	);
 
@@ -13,7 +13,7 @@ export const getProducts = async (_page: number, _limit: number) => {
 export const getProductsByViews = async () => {
 	const products = await axios.get(
 		`${
-			process.env.REACT_APP_VERCEL_ENV_AUTH0_DOMAIN || "http://localhost:33714"
+			process.env.REACT_APP_VERCEL_ENV_API_DOMAIN || "http://localhost:33714"
 		}/products/order/views/`
 	);
 
