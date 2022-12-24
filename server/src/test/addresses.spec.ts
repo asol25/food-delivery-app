f73a494b2fe2cab3e4087712b27f35fcf75d5e86
asol25 <usool.203@gmail.com>
2022-12-24 14:38:49 +0700

    Testing addressess

---
+++ server/src/test/addresses.spec.ts
@@ -0,0 +1,79 @@
+import { DataSource } from "typeorm";
+import * as pathDataSourceConfig from "../../data-source";
+import { AddressesController } from "./../controllers/addresses.controller";
+import { AddressesRepository } from "./../models/repositories/addresses.repository";
+import { Addresses } from "./../models/tables/addresses";
+import { AddressesService } from "./../providers/addresses.service";
+
+describe("Users Testing", () => {
+	let service: AddressesService;
+	let controller: AddressesController;
+	let repository: AddressesRepository;
+	let dataSource: DataSource;
+
+	beforeEach(() => {
+		dataSource = pathDataSourceConfig.default;
+		repository = new AddressesRepository(dataSource);
+		service = new AddressesService(repository);
+		controller = new AddressesController(service);
+	});
+
+	describe("get addresses of user", () => {
+		it("should return an addresses of user", async () => {
+			const result = {
+				id: 1,
+				addresses_one: "HCM CITY",
+				addresses_two: "HCM CITY",
+			};
+
+			jest
+				.spyOn(service, "getAddressesByUserId")
+				.mockImplementation(() => Promise.resolve(result as Addresses));
+
+			expect(await controller.getAddressesByUserId({ userId: 1 })).toBe(result);
+		});
+	});
+
+	describe("create addresses of user", () => {
+		it("should return an addresses of user", async () => {
+			const result = {
+				addresses_one: "HCM CITY",
+				addresses_two: "HCM CITY",
+			};
+
+			jest
+				.spyOn(service, "createAddresses")
+				.mockImplementation(() => Promise.resolve(result as Addresses));
+
+			expect(
+				await controller.createAddresses({
+					userId: 1,
+					addresses_one: "HCM CITY",
+					addresses_two: "HCM CITY",
+				})
+			).toBe(result);
+		});
+	});
+
+	describe("update addresses of user", () => {
+		it("should return an addresses of user", async () => {
+			const result = {
+				id: 1,
+				addresses_one: "HCM CITY",
+				addresses_two: "HCM CITY",
+			};
+
+			jest
+				.spyOn(service, "updateAddresses")
+				.mockImplementation(() => Promise.resolve(result as Addresses));
+
+			expect(
+				await controller.updateAddresses({
+					userId: 1,
+					addresses_one: "HCM CITY",
+					addresses_two: "HCM CITY",
+				})
+			).toBe(result);
+		});
+	});
+});
