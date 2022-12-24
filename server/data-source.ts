6771ccce98f651b92e02e0dd6abdaebd675d7ffe
asol25 <usool.203@gmail.com>
2022-12-24 12:11:45 +0700

    Config data-source

--- server/data-source.ts
+++ server/data-source.ts
@@ -1,22 +1,22 @@
 // ./data-source.ts
-import { DataSource } from 'typeorm';
-import * as dotenv from 'dotenv';
-import * as path from 'path';
+import * as dotenv from "dotenv";
+import * as path from "path";
+import { DataSource } from "typeorm";
 
 dotenv.config();
 
 const NODE_ENV = process.env.NODE_ENV;
 
 export default new DataSource({
-	type: 'mysql',
+	type: "postgres",
 	host: process.env[`${NODE_ENV}_DB_HOST`] as string,
 	port: Number(process.env[`${NODE_ENV}_DB_PORT`]) as number,
 	username: process.env[`${NODE_ENV}_DB_USERNAME`] as string,
 	database: process.env[`${NODE_ENV}_DB_DATABASE`] as string,
 	password: process.env[`${NODE_ENV}_DB_PASSWORD`] as string,
 	entities: [
-		path.join(__dirname, './src/models/tables/*.ts'),
-		path.join(__dirname, './src/models/tables/*.js'),
+		path.join(__dirname, "./src/models/tables/*.ts"),
+		path.join(__dirname, "./src/models/tables/*.js"),
 	],
 	synchronize: false,
 	logging: true,
