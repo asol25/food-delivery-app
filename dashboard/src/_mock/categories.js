f465348b6d33cd8e67f3adc74ab5ee43b9ef0715
asol25 <usool.203@gmail.com>
2022-12-26 21:13:25 +0700

    Fix loop useEffect

--- dashboard/src/_mock/categories.js
+++ dashboard/src/_mock/categories.js
@@ -1,9 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const getCategories = async () => {
-  const categories = await axios.get(
-    `${process.env.REACT_APP_SERVER_URL || 'http://localhost:33714'}/categories/get/all`
-  );
+	const categories = await axios.get(
+		`${
+			process.env.REACT_APP_SERVER_URL || "http://localhost:33714"
+		}/categories/get/all`
+	);
 
-  return categories;
+	return categories;
 };
--- dashboard/src/sections/@dashboard/products/ProductFilterSidebar.js
+++ dashboard/src/sections/@dashboard/products/ProductFilterSidebar.js
@@ -91,7 +91,7 @@ export default function ShopFilterSidebar({
 		return () => {
 			isChecked = false;
 		};
-	}, [filterCategories]);
+	}, []);
 
 	return (
 		<>
